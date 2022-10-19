4a9e27213bc7531055e8c7eae9fb1c2291e7b2e3
Anan <ranan22@tbc.edu.np>
2022-10-19 12:14:08 +0545

    Q.4

--- Q.4.py
+++ Q.4.py
@@ -2,3 +2,9 @@
 # first count the sweets and then divide them according to how many pupils attend
 # that day. Write a program that will tell the teacher how many sweets to give to each
 # pupil, and how many she will have left over.
+sweets=int(input("How many sweets? "))
+pupils=int(input("How many students? "))
+div=sweets//pupils
+left=sweets%pupils
+print("If there are %d sweets and %d students,"%(sweets,pupils))
+print("Then each pupils will get %d sweets and teacher will be left with %d sweets."%(div,left))
